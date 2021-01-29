1c77a8a56b36352a459a47b9d70e6637dd4a4433
anleus <tomas.rui598@gmail.com>
2021-01-29 22:41:19 +0100

    paloma vuela

---
+++ Assets/Scripts/Pigeon/PigeonMovement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField] private float hSpeed;
+    [SerializeField] private float vSpeed;
+    [SerializeField] private float flutterSpeed;
+
+    private bool isFluttering = false;
+    private bool isFreeFalling = false;
+
+    public Vector2 moveInput;
+    private Vector3 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        GetMoveInputs();
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isFluttering)
+        {
+            if (!isFreeFalling)
+            {
+                StartCoroutine(Flutter());
+            }
+        }
+
+        FreeFall();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();   
+    }
+
+    private void GetMoveInputs()
+    {
+        moveInput = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"));
+        moveInput.Normalize();
+    }
+
+    private void Movement()
+    {
+        movement.x = moveInput.x * hSpeed;
+        
+        if (!isFreeFalling)
+        {
+            movement.y = moveInput.y * vSpeed;
+        }
+        else
+        {
+            movement.y = 0f;
+        }
+
+        movement *= Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
+    }
+
+    private IEnumerator Flutter()
+    {
+        isFluttering = true;
+
+        rb.velocity = new Vector3(rb.velocity.x, flutterSpeed, rb.velocity.z);
+        yield return new WaitForSeconds(0.5f);
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        isFluttering = false;
+    }
+
+    private void FreeFall()
+    {
+        if (!isFluttering && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isFreeFalling = true;
+            rb.useGravity = true;
+        }
+
+        if (!isFluttering && Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isFreeFalling = false;
+            rb.useGravity = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Pigeon/PigeonTakeObject.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonTakeObject : MonoBehaviour
+{
+    public Transform Claws;
+
+    private bool canInteract = false;
+    private bool isHolding = false;
+
+    private RaycastHit focus;
+    private Rigidbody nearestItem;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canInteract)
+        {
+            nearestItem = focus.collider.transform.GetComponent<Rigidbody>();
+
+            if (nearestItem != null)
+            {
+                if (!isHolding)
+                {
+                    isHolding = true;
+                    TakeItem();
+                    Debug.Log("coger");
+                }
+                else
+                {
+                    isHolding = false;
+                    DropItem();
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CheckDownstairs();
+    }
+
+    private void CheckDownstairs()
+    {
+        float distance = 1.5f;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out focus, distance) && focus.collider.transform.CompareTag("Item"))
+        {
+            canInteract = true;
+        }
+        else
+        {
+            canInteract = false;
+        }
+    }
+
+    private void TakeItem()
+    {
+        nearestItem.useGravity = false;
+        nearestItem.transform.parent = Claws;
+    }
+
+    private void DropItem()
+    {
+        nearestItem.useGravity = true;
+        nearestItem.isKinematic = false;
+        nearestItem.transform.parent = null;
+    }
+}
