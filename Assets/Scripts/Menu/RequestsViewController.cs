1d4c4a5afd09fdbece8b92088b5f7e7aef44bcae
n2tmad <39457122+n2tmad@users.noreply.github.com>
2021-01-30 18:37:02 +0100

    Lista de items en UI, fix de items que se usaban dos veces

---
+++ Assets/Scripts/Menu/RequestsViewController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Palomas.Menu
+{
+    public class RequestsViewController : MonoBehaviour
+    {
+        private GameEvents GameEvents => GameEvents.Instance;
+
+        [SerializeField]
+        private Transform ViewHolder;
+
+        [SerializeField]
+        private GameObject ViewPrefab;
+
+        private IDictionary<string, RequestView> RequestViews;
+
+        private void Start()
+        {
+            RequestViews = new Dictionary<string, RequestView>();
+
+            GameEvents.RequestObtained += (sender, args) => AddRequestToView(args);
+            GameEvents.RequestCompleted += (sender, args) => SetRequestAsCompleted(args);
+        }
+
+        private void AddRequestToView(RequestEventArgs args)
+        {
+            GameObject viewObject = GameObject.Instantiate(ViewPrefab, ViewHolder);
+            RequestView requestView = viewObject.GetComponent<RequestView>();
+            requestView.SetRequestId(args.RequestId);
+
+            RequestViews.Add(args.RequestId, requestView);
+        }
+
+        private void SetRequestAsCompleted(RequestItemEventArgs args)
+        {
+            //TODO animate
+            if(RequestViews.TryGetValue(args.RequestId, out RequestView requestView))
+            {
+                Destroy(requestView.gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Requests/RequestTrigger.cs
+++ Assets/Scripts/Requests/RequestTrigger.cs
@@ -22,7 +22,7 @@ namespace Palomas.Requests
                 ItemController item = other.gameObject.GetComponent<ItemController>();
                 if (Request.IsInUse() && !item.GetItemstatus() && item.GetItemId().Equals(Request.GetItemId()))
                 {
-                    GameEvents.OnRequestCompleted(Request.GetId());
+                    GameEvents.OnRequestCompleted(Request.GetId(), Request.GetItemId());
                 }
             }
         }
