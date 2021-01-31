1b8dc7ef872fc60355e6e2e3f8fadb108ddd8e79
n2tmad <39457122+n2tmad@users.noreply.github.com>
2021-01-31 15:58:51 +0100

    edificio nuevo en level

---
+++ Assets/Scripts/Audio/RequestChangedSFXController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Palomas.Audio
+{
+    public class RequestChangedSFXController : SFXController
+    {
+        private GameEvents GameEvents => GameEvents.Instance;
+
+        [SerializeField]
+        private AudioClip HeySound;
+
+        private void Start()
+        {
+            GameEvents.RequestChanged += (sender, args) => { PlayClip(HeySound); };
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/SFXController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Palomas.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public abstract class SFXController : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource AudioSource;
+
+        protected void PlayClip(AudioClip clip)
+        {
+            if(AudioSource != null)
+            {
+                AudioSource.PlayOneShot(clip);
+            }
+        }
+
+        protected void PlayRandomClip(ICollection<AudioClip> clips)
+        {
+            AudioClip randomClip = GameUtils.RandomElement(clips);
+
+            PlayClip(randomClip);
+        }
+
+        protected void Stop()
+        {
+            AudioSource.Stop();
+        }
+    }
+}
